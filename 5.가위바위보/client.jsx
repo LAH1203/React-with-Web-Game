55e466a32adc36659375269b1a1005e5cb991f7e
LAH1203 <lah1203@naver.com>
2021-06-28 16:38:40 +0900

    5-1 ~ 5-6 (2021-06-28)

---
+++ 5.가위바위보/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+import RSP from './RSP_hooks';
+
+ReactDom.render(<RSP />, document.querySelector('#root'));
---
+++ 5.가위바위보/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    name: 'rsp-setting',
+    mode: 'development',    // 실서비스에서는 production
+    devtool: 'eval',    // 실서비스에서는 hidden-source-map
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    
+    // 입력
+    entry: {
+        app: ['./client'],
+    },
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: [['@babel/preset-env', {
+                    targets: {
+                        browsers: ['> 1% in KR'],
+                    },
+                    debug: true,
+                }],
+                '@babel/preset-react'
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel',
+                ],
+            },
+        }],
+    },
+
+    plugins: [
+        new RefreshWebpackPlugin(),
+    ],
+
+    // 출력
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath: '/dist/',
+    },
+
+    devServer: {
+        publicPath: '/dist/',
+        hot: true
+    },
+};
